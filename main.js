99ba387e4d08350797c9845c8db75a49aa7ef513
Shreya Modi <shreyamodi@Shreyas-MacBook-Pro.local>
2022-10-13 19:43:52 -0400

    barchart

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// setting up constants
 const FRAME_HEIGHT = 500;
 const FRAME_WIDTH = 500;
 const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
@@ -13,6 +14,7 @@ const FRAME1 =
 			.attr('class', 'frame')
 		.append('g')
 			.attr("transform", `translate(${MARGINS.left},${MARGINS.top})`);
+
 
 d3.csv('data/scatter-data.csv').then( function(data) {
 	// Add X axis
@@ -57,3 +59,62 @@ d3.csv('data/scatter-data.csv').then( function(data) {
 			}) 
 
 })
+
+// making barchart 
+const FRAME2 = d3.select("#Barchart")
+                .append("svg")
+                    .attr("width", FRAME_WIDTH)
+                    .attr("height", FRAME_HEIGHT)
+                    .attr("class", "frame"); 
+
+// building 
+function build_bar_chart() {
+    d3.csv('bar-data.csv').then((data) => {
+
+        // creating x scale 
+        const X_SCALE = d3.scaleBand() // for categorical data 
+                            .range([0, VIS_WIDTH])
+                            .domain(data.map(d => d.Category))
+                            .padding(0.2); 
+        // Adding x axis
+        FRAME2.append("g")
+                .attr("transform", "translate(" + MARGINS.left +
+                    "," + (VIS_HEIGHT + MARGINS.top) + ")")
+                .call(d3.axisBottom(X_SCALE));  
+
+        // creating Y scale 
+        const Y_SCALE = d3.scaleLinear()
+                            .domain([0, 100000])
+                            .range([VIS_HEIGHT, 0]); 
+
+        // Adding Y axis 
+        FRAME2.append("g")
+                .attr("transform", "translate(" + MARGINS.left +
+                    "," + MARGINS.top + ")")
+                .call(d3.axisLeft(Y_SCALE));  
+
+        // Add bars 
+        FRAME2.selectAll("bars")
+                .data(data)
+                .enter()
+                .append("rect")
+                    .attr("x", (d) => {
+                        // x pos depends on category 
+                        return (X_SCALE(d.Category) + MARGINS.left); 
+                    })
+                    .attr("y", (d) => {
+                        // start of rect depends on value 
+                        return (Y_SCALE(d.Value) + MARGINS.top);
+                    })
+                    .attr("height", (d) => {
+                        // height of bar should be height of vis - value
+                        return (VIS_H - Y_SCALE(d.Value));
+                    })
+                    .attr("width", X_SCALE.bandwidth()) // width comes from X_SCALE for free
+                    .attr("class", (d) => {
+                        return (d.Category + " bar"); 
+                    }); 
+    })
+}
+
+build_bar_chart();
